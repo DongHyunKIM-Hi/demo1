dcc90efbf9079fac29b3dd1b03e4d938cfa9099e
Viva <happybird00@naver.com>
2025-07-29 14:56:38 +0900

    test

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,2 +1,13 @@
-package com.example.demo;public class TestController {
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hi")
+    public String test() {
+        return "짜잔! 내가 만든 서버";
+    }
 }
